9fdd85c8ea3db4fa84e23a56f0ad61824c0d25e1
mpcayres <mp_cayres96@hotmail.com>
2016-11-01 09:18:33 -0200

    Limpando um pouco

--- app/src/main/java/com/example/ingrid/myapplication/MenuPrincipalActivity.java
+++ app/src/main/java/com/example/ingrid/myapplication/MenuPrincipalActivity.java
@@ -12,43 +12,46 @@ public class MenuPrincipalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
-
-        Intent intent = getIntent();
     }
+
     public void inserir(View view) {
         // Do something in response to button
         Intent intent = new Intent(this, TipoActivity.class);
         startActivity(intent);
 
     }
+
     public void calendario(View view) {
         // Do something in response to button
         Intent intent = new Intent(this, CalendarioActivity.class);
         startActivity(intent);
-
     }
+
     public void hoje(View view) {
         // Do something in response to button
         Intent intent = new Intent(this, HojeActivity.class);
         startActivity(intent);
 
     }
+
     public void tudo(View view) {
         // Do something in response to button
         Intent intent = new Intent(this, TudoActivity.class);
         startActivity(intent);
 
     }
+
     public void estatistica(View view) {
         // Do something in response to button
         Intent intent = new Intent(this, EstatisticaActivity.class);
         startActivity(intent);
 
     }
-    public void recorrente(View view) {
-        // Do something in response to button
-        Intent intent = new Intent(this, Recorrente.class);
-        startActivity(intent);
 
+    public void conf(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, ConfActivity.class);
+        startActivity(intent);
     }
+
 }
