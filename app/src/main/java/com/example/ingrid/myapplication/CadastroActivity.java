3893fa7f0f8e0ab160521d8ee9822763831d51e4
mpcayres <mp_cayres96@hotmail.com>
2016-11-04 14:40:21 -0200

    Uns detalhes
    Te ajudando, Ingrid. Pra pegar as info das caixas de texto

--- app/src/main/java/com/example/ingrid/myapplication/CadastroActivity.java
+++ app/src/main/java/com/example/ingrid/myapplication/CadastroActivity.java
@@ -4,16 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class CadastroActivity extends AppCompatActivity {
+    private EditText editLogin;
+    private EditText editSenha;
+    private EditText editConfSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
 
-        Intent intent = getIntent();
-
+        editLogin = (EditText) findViewById(R.id.editLogin);
+        editSenha = (EditText) findViewById(R.id.editSenha);
+        editConfSenha = (EditText) findViewById(R.id.editConfSenha);
     }
     public void salvar(View view) {
         // Do something in response to button
--- app/src/main/java/com/example/ingrid/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ingrid/myapplication/MainActivity.java
@@ -4,13 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editLogin;
+    private EditText editSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editLogin = (EditText) findViewById(R.id.editLogin);
+        editSenha = (EditText) findViewById(R.id.editSenha);
     }
 
     public void entrar(View view) {
