ae9d194c27d757457be4dfdf0d3b8e3787b97864
mpcayres <mp_cayres96@hotmail.com>
2016-11-04 19:45:33 -0200

    Barra lateral, calendário, ajustes gerais

--- app/src/main/java/com/example/ingrid/myapplication/TipoActivity.java
+++ app/src/main/java/com/example/ingrid/myapplication/TipoActivity.java
@@ -4,15 +4,42 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
-public class TipoActivity extends AppCompatActivity {
+import com.example.ingrid.myapplication.banco.SuperTela;
+
+import it.sephiroth.android.library.tooltip.Tooltip;
+
+public class TipoActivity extends SuperTela {
+    private ImageButton help_tip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tipo);
+        super.onCreate(savedInstanceState);
 
-        Intent intent = getIntent();
+        help_tip = (ImageButton) findViewById(R.id.help_tip);
+
+        if(help_tip != null){
+            help_tip.setOnClickListener( new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Tooltip.make(activity,
+                            new Tooltip.Builder(101)
+                                    .anchor(help_tip, Tooltip.Gravity.BOTTOM)
+                                    .closePolicy(new Tooltip.ClosePolicy()
+                                            .insidePolicy(true, true)
+                                            .outsidePolicy(true, false), 200000)
+                                    .text(getResources().getString(R.string.tipo_tip))
+                                    .fitToScreen(true)
+                                    .withArrow(true)
+                                    .withOverlay(true)
+                                    .floatingAnimation(Tooltip.AnimationBuilder.SLOW)
+                                    .build()
+                    ).show();
+                }
+            });
+        }
     }
     public void periodica(View view) {
         // Do something in response to button
