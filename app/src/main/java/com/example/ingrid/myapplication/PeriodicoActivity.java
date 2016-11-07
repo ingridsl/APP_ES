227e98944ae472c340170de3e9b9e43ec9e04927
ingridsl <inus2agome@gmail.com>
2016-11-07 11:13:51 -0200

    frequencia de atividades periodicas parcialmente adicionada

--- app/src/main/java/com/example/ingrid/myapplication/PeriodicoActivity.java
+++ app/src/main/java/com/example/ingrid/myapplication/PeriodicoActivity.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -30,6 +31,7 @@ public class PeriodicoActivity extends SuperTela {
     private EditText editHoraFin;
     private EditText editNotas;
     private TextView textFrequencia;
+    public boolean segunda, terca, quarta, quinta, sexta, sabado, domingo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,15 +43,14 @@ public class PeriodicoActivity extends SuperTela {
         editHoraIni = (EditText) findViewById(R.id.editHoraIni);
         editHoraFin = (EditText) findViewById(R.id.editHoraFin);
         editNotas = (EditText) findViewById(R.id.editNotas);
-
         textFrequencia = (TextView) findViewById(R.id.textFrequencia);
 
         if(textFrequencia != null){
             textFrequencia.setOnClickListener( new View.OnClickListener()  {
                 @Override
-                public Dialog onCreateDialog(Bundle savedInstanceState) {
+                public void onClick(View v) {
                     final ArrayList mSelectedItems = new ArrayList();  // Where we track the selected items
-                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                    AlertDialog.Builder builder = new AlertDialog.Builder(PeriodicoActivity.this);
                     // Set the dialog title
                     builder.setTitle(R.string.frequencia)
                             // Specify the list array, the items to be selected by default (null for none),
@@ -57,11 +58,18 @@ public class PeriodicoActivity extends SuperTela {
                             .setMultiChoiceItems(R.array.week_days, null,
                                     new DialogInterface.OnMultiChoiceClickListener() {
                                         @Override
-                                        public void onClick(DialogInterface dialog, int which,
-                                                            boolean isChecked) {
+                                        public void onClick(DialogInterface dialog, int which, boolean isChecked) {
                                             if (isChecked) {
                                                 // If the user checked the item, add it to the selected items
+                                                // 0 - segunda
+                                                // 1 - terca
+                                                // 2 - quarta
+                                                // 3 - quinta
+                                                // 4 - sexta
+                                                // 5 - sabado
+                                                // 6 - domingo
                                                 mSelectedItems.add(which);
+                                                Log.d ("TAG","selected item" + which);
                                             } else if (mSelectedItems.contains(which)) {
                                                 // Else, if the item is already in the array, remove it
                                                 mSelectedItems.remove(Integer.valueOf(which));
@@ -69,21 +77,65 @@ public class PeriodicoActivity extends SuperTela {
                                         }
                                     })
                             // Set the action buttons
-                            .setNegativeButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                                 @Override
 
                                 public void onClick(DialogInterface dialog, int id) {
                                     // User clicked OK, so save the mSelectedItems results somewhere
                                     // or return them to the component that opened the dialog
+                                    if(mSelectedItems.contains(0)){
+                                        segunda = true;
+                                    }else{
+                                        segunda = false;
+                                    }
+
+                                    if(mSelectedItems.contains(1)){
+                                        terca = true;
+                                    }else{
+                                        terca = false;
+                                    }
+
+                                    if(mSelectedItems.contains(2)){
+                                        quarta = true;
+                                    }else{
+                                        quarta = false;
+                                    }
+
+                                    if(mSelectedItems.contains(3)){
+                                        quinta = true;
+                                    }else{
+                                        quinta = false;
+                                    }
+
+                                    if(mSelectedItems.contains(4)){
+                                        sexta = true;
+                                    }else{
+                                        sexta = false;
+                                    }
+
+                                    if(mSelectedItems.contains(5)){
+                                        sabado = true;
+                                    }else{
+                                        sabado = false;
+                                    }
+
+                                    if(mSelectedItems.contains(6)){
+                                        domingo = true;
+                                    }else{
+                                        domingo = false;
+                                    }
+
+
                                 }
                             })
-                            .setPositiveButton(R.string.cancelar, new DialogInterface.OnClickListener() {
+                            .setNegativeButton(R.string.cancelar, new DialogInterface.OnClickListener() {
                                 @Override
                                 public void onClick(DialogInterface dialog, int id) {
                                 }
                             });
 
-                    return builder.create();
+                    AlertDialog alert11 = builder.create();
+                    alert11.show();
                 }
             });
         }
@@ -93,12 +145,12 @@ public class PeriodicoActivity extends SuperTela {
 
         Intent intent = new Intent(this, RecorrenteActivity.class);
 
+        Periodico periodico = new Periodico();
         String Nome = editNome.getText().toString();
         String HoraIni = editHoraIni.getText().toString();
         String HoraFin = editHoraFin.getText().toString();
         String Notas = editNotas.getText().toString();
 
-        Periodico periodico = new Periodico();
         periodico.setNome(Nome);
 
         SimpleDateFormat formatTime = new SimpleDateFormat("HH:mm:ss", Locale.ENGLISH);
@@ -118,6 +170,14 @@ public class PeriodicoActivity extends SuperTela {
         periodico.setHoraFinal ((java.sql.Time) HoraFinal);
 
         periodico.setNome(Notas);
+        periodico.setSegunda(segunda);
+        periodico.setTerca(terca);
+        periodico.setQuarta(quarta);
+        periodico.setQuinta(quinta);
+        periodico.setSexta(sexta);
+        periodico.setSabado(sabado);
+        periodico.setDomingo(domingo);
+
 
         DataBaseHelper.getInstance(this.getApplicationContext()).addPeriodico(periodico);
 
@@ -130,7 +190,6 @@ public class PeriodicoActivity extends SuperTela {
         Intent intent = new Intent(this, UnicoActivity.class);
 
         String Nome = editNome.getText().toString();
-       // String Data = editData.getText().toString();
         String HoraIni = editHoraIni.getText().toString();
         String HoraFin = editHoraFin.getText().toString();
         String Notas = editNotas.getText().toString();
@@ -155,6 +214,13 @@ public class PeriodicoActivity extends SuperTela {
         periodico.setHoraFinal ((java.sql.Time) HoraFinal);
 
         periodico.setNome(Notas);
+        periodico.setSegunda(segunda);
+        periodico.setTerca(terca);
+        periodico.setQuarta(quarta);
+        periodico.setQuinta(quinta);
+        periodico.setSexta(sexta);
+        periodico.setSabado(sabado);
+        periodico.setDomingo(domingo);
 
         DataBaseHelper.getInstance(this.getApplicationContext()).addPeriodico(periodico);
 
